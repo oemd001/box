a751251e286e87594341c73f5c1a146814c8141b
Ubuntu <ubuntu@ip-172-31-22-116.us-west-1.compute.internal>
2023-05-25 23:10:35 +0000

    Made modifications to DL code

--- master_node.py
+++ master_node.py
@@ -1,5 +1,16 @@
 import torch
 import torch.distributed as dist
+import signal
+import sys
+
+stop = False
+
+def signal_handler(sig, frame):
+    print('You pressed Ctrl+C!')
+    global stop
+    stop = True
+
+signal.signal(signal.SIGINT, signal_handler)
 
 def main(rank, world_size):
     print(f"Initializing process group for rank {rank} out of {world_size} total processes.")
@@ -7,15 +18,16 @@ def main(rank, world_size):
         "gloo",
         rank=rank,
         world_size=world_size,
-        init_method='tcp://0.0.0.0:6436' 
+        init_method='tcp://127.0.0.1:6435'
     )
     print("Process group initialized. Ready to accept connections.")
 
 if __name__ == "__main__":
-    world_size = 2 
+    world_size = 2
     print("Spawning subprocesses...")
     torch.multiprocessing.spawn(main, args=(world_size,), nprocs=world_size, join=True)
     print("All subprocesses have been spawned.")
     
-    import time
-    time.sleep(60)  # Keep the script running for 60 seconds
+    while not stop:
+        pass  # keep the script running
+
